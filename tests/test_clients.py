d8793d82a3c4773fa3ef16d2a26ce7ba163c9e8e
adrianoesch <4437435+adrianoesch@users.noreply.github.com>
2025-02-06 08:44:11 +0100

    add valid import test

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,7 +1,20 @@
 import pytest
 from unittest.mock import patch
 
-def test_import_error():
+
+def test_valid_import():
+
+    with patch.dict('sys.modules', {'snowflake.connector': None}):
+        error = None
+        try:
+            from chm_utils.clients import Snowflake
+        except Exception as e:
+            error = e
+        
+    assert error is None
+
+
+def test_import_error_at_init():
 
     with patch.dict('sys.modules', {'snowflake.connector': None}):
         error = None
