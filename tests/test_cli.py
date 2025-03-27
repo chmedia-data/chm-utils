497f2b4966592fafb6b26a4b4eb13c9fa64e1fac
Paschalis Dimitriou <paschalis.dimitriou@gmail.com>
2025-03-27 11:21:12 +0100

    improve cli by adding flag for file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from unittest.mock import patch
 from chm_utils import cli
 
@@ -12,9 +13,27 @@ def test_get_env(capsys):
     with patch('sys.argv', ['cli.py', 'sls.get_env', '-sep','functions.test_function.environment']):
         cli.main()
 
-    sysout = capsys.readouterr().out.strip()
-    assert sysout == "TEST_OPT_VAR=test TEST_HARDCODED=hardcoded TEST_SPECIAL_CHARS='---- rsa begin ----\nasdfasdfasdf\n---- rsa end ----\n'"
-    assert capsys.readouterr().out.strip() == "TEST_OPT_VAR=test TEST_HARDCODED=hardcoded"
+    output = capsys.readouterr().out.strip()
+    assert "export TEST_OPT_VAR=test" in output
+    assert "export TEST_HARDCODED=hardcoded" in output
+    assert "export TEST_SPECIAL_CHARS='---- rsa begin ----\nasdfasdfasdf\n---- rsa end ----\n'" in output
+
+
+def test_get_env_to_file():
+    os.environ['SLS_CONFIG_PATH'] = 'tests/fixtures/serverless_small.yml'
+    os.environ['STAGE'] = 'test'
+    os.environ['TEST_NOT_OVERWRITE'] = 'not_overwritten'
+
+    with tempfile.NamedTemporaryFile(mode='w+') as temp_file:
+        with patch('sys.argv', ['cli.py', 'sls.get_env', '-sep', 'functions.test_function.environment', 
+                                '-of', temp_file.name]):
+            cli.main()
+        
+        temp_file.seek(0)
+        content = temp_file.read()
+        assert "export TEST_OPT_VAR=test" in content
+        assert "export TEST_HARDCODED=hardcoded" in content
+        assert "export TEST_SPECIAL_CHARS='---- rsa begin ----\nasdfasdfasdf\n---- rsa end ----\n'" in content
 
 
 def test_version(capsys):
